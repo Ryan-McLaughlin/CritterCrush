eb42ef6603327978414605135b2564e4c67d9802
Ryan McLaughlin <ryan.mclaughlin00@gmail.com>
2024-09-03 17:46:00 -0500

    Area01_Scene
    added
     - environment
     - critter
     - critter script

--- Assets/Scripts/Critter.cs
+++ Assets/Scripts/Critter.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class Critter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse clicked on " + this.name);
+        }
     }
 }
